1875eb12d27e44e8166fcd011289008c928aecc5
Brandon <blackestnapkin@gmail.com>
2016-06-30 19:35:50 -0500

    fixed header redirect

--- form.php
+++ form.php
@@ -56,22 +56,11 @@ if(isset($_POST['name'], $_POST['email'], $_POST['message']))
     $error = 'Something went wrong.';
 }
 
-/*
-if(empty($errors))
-{
-    $to = $myemail;
-    $email_subject = "Contact form submission: $name";
-    $email_body = "You have received a new message. " . " Here are the details:\n Name: $name \n " . "Email: $email_address\n Message \n $message";
-    $headers = "From: $myemail\n";
-    $headers .= "Reply-To: $email_address";
-    mail($to,$email_subject,$email_body,$headers);
-}
-*/
 $_SESSION['error'] = $error;
 $_SESSION['name'] = $name;
 $_SESSION['email'] = $email;
 $_SESSION['message'] = $message;
 
-header('Location: reboot.php');
+header('Location: index.php');
 
 ?>
