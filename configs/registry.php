e0aa66686e4c52feb9804cab6f51e43cf63e92c9
Nickel <brandon@nickel.Home>
2019-02-16 17:00:02 -0600

    Updated composer and registry to work with intersect-blog

--- configs/registry.php
+++ configs/registry.php
@@ -1,17 +1,10 @@
 <?php
 
 use Intersect\Application;
-use Intersect\Database\Connection\Connection;
 use Intersect\Blog\Commands\InstallBlogCommand;
 use Intersect\Blog\Commands\UpgradeBlogCommand;
-use Intersect\Database\Connection\ConnectionFactory;
-use Intersect\Database\Connection\ConnectionSettings;
 
-$application = Application::instance();
-
-$databaseConfig = $application->getRegisteredConfigs('database');
-$connectionSettings = new ConnectionSettings($databaseConfig['host'], $databaseConfig['username'], $databaseConfig['password'], $databaseConfig['port'], $databaseConfig['name']);
-$connection = ConnectionFactory::get($databaseConfig['driver'], $connectionSettings);
+$connection = Application::instance()->getConnection();
 
 return [
     'classes' => [],
