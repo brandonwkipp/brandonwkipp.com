54dffbc20eef228a6f86674de45601a50538b415
Nickel <brandon@nickel.Home>
2019-04-04 00:42:29 -0500

    Deleted old files, added link to homepage

--- old/clownsword/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-require_once 'autoloader.php';
-
-$database = new \Models\MySQLiDatabase('localhost', 'soundsword_admin', 'Lemongrab80!', 'soundsword');
-//$database = new \Models\MySQLiDatabase('127.0.0.1', 'root', '', 'soundsword');
-$userService = new \Services\UserService($database);
-$soundSwordService = new \Services\SoundSwordService($database);
