9b40f65a32fae00ee5bf015c130c0b7bbf7ad69f
Brandon Kipp <brandonkipp@MacBook-Pro.local>
2020-06-20 14:00:10 -0500

    refactor: Updated Blog component to pull from Contnetful

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,24 +1,23 @@
+import { BLOCKS } from '@contentful/rich-text-types';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { StaticQuery, graphql } from 'gatsby';
-import Markdown from 'markdown-to-jsx';
 import React from 'react';
-import { Card } from 'reactstrap';
+import {
+  Card, Col, Container, Row,
+} from 'reactstrap';
 
 import './index.scss';
 
 const BLOG_POSTS = graphql`
   query {
-    allMarkdownRemark(
-      filter: { frontmatter: { type: { eq: "blog" }} },
-      sort: { order: DESC, fields: [frontmatter___date] }
-    ) {
+    allContentfulBlogPost(sort: {order: DESC, fields: date}) {
       edges {
         node {
-          id
-          frontmatter {
-            date(formatString: "MMMM Do, YYYY")
-            title
+          body {
+            json
           }
-          html
+          date(formatString: "MMMM Do, YYYY")
+          title
         }
       }
     }
@@ -28,19 +27,49 @@ const BLOG_POSTS = graphql`
 const Blog = () => (
   <StaticQuery
     query={BLOG_POSTS}
-    render={(data) => (
-      <>
-        {data.allMarkdownRemark.edges.map((post) => (
-          <Card className="blog-post border-0 rounded-0">
-            <h1 className="mt-3 text-center">{post.node.frontmatter.title}</h1>
-            <h5 className="text-center">{post.node.frontmatter.date}</h5>
-            <div className="mb-3 mx-auto w-75">
-              <Markdown>{post.node.html}</Markdown>
-            </div>
-          </Card>
-        ))}
-      </>
-    )}
+    render={(data) => {
+      const options = {
+        renderNode: {
+          [BLOCKS.EMBEDDED_ENTRY]: (node) => {
+            const { blockquote, file } = node.data.target.fields;
+            const { title } = file['en-US'].fields.title['en-US'];
+            const { url } = file['en-US'].fields.file['en-US'];
+
+            return (
+              <Container className="my-5" fluid>
+                <Row>
+                  <Col className="text-center">
+                    <img
+                      alt={title}
+                      src={url}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="text-center">
+                    {blockquote['en-US']}
+                  </Col>
+                </Row>
+              </Container>
+            );
+          },
+        },
+      };
+
+      return (
+        <>
+          {data.allContentfulBlogPost.edges.map((blog) => (
+            <Card className="blog-post border-0 rounded-0" key={blog.node.title}>
+              <h1 className="mt-3 text-center">{blog.node.title}</h1>
+              <h5 className="text-center">{blog.node.date}</h5>
+              <div className="mb-3 mx-auto w-75">
+                {documentToReactComponents(blog.node.body.json, options)}
+              </div>
+            </Card>
+          ))}
+        </>
+      );
+    }}
   />
 );
 
