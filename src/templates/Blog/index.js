38b4f0969d1f7cc1449f581301d4c0947e8fda8f
Brandon Kipp <brandonkipp@MacBook-Pro.local>
2020-06-20 13:59:39 -0500

    refactor: Cleaned up blog template

--- src/templates/Blog/index.js
+++ src/templates/Blog/index.js
@@ -1,5 +1,5 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { graphql } from 'gatsby';
-import Markdown from 'markdown-to-jsx';
 import propTypes from 'prop-types';
 import React from 'react';
 import { Card } from 'reactstrap';
@@ -11,30 +11,28 @@ import Layout from '@components/Layout';
 import '@components/Blog/index.scss';
 
 export const PAGE_QUERY = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
-      frontmatter {
-        date(formatString: "MMMM Do, YYYY")
-        path
-        title
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug } ) {
+      body {
+        json
       }
+      date(formatString: "MMMM Do, YYYY")
+      title
     }
   }
 `;
 
 export default function Template({ data }) {
-  const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
+  const { contentfulBlogPost } = data;
 
   return (
     <Layout>
       <Header />
       <Card className="blog-post border-0 rounded-0">
-        <h1 className="mt-3 text-center">{frontmatter.title}</h1>
-        <h6 className="text-center">{frontmatter.date}</h6>
+        <h1 className="mt-3 text-center">{contentfulBlogPost.title}</h1>
+        <h6 className="text-center">{contentfulBlogPost.date}</h6>
         <div className="mb-3 mx-auto w-75">
-          <Markdown>{html}</Markdown>
+          {documentToReactComponents(contentfulBlogPost.body.json)}
         </div>
       </Card>
       <Footer />
