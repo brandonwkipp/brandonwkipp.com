1084db4ea39f1e4d0565b280e52e80c5c0536203
Brandon Kipp <brandonwkipp@gmail.com>
2020-08-12 21:30:55 -0500

    refactor: Added migrations

---
+++ migrations/3.js
@@ -0,0 +1,12 @@
+module.exports = function runMigration(migration) {
+  const neighbor = migration.editContentType('neighbor');
+
+  neighbor
+    .name('Loud machine')
+    .description('Who\'s an asshole? She is!');
+
+  neighbor.createField('loudnessLevel')
+    .type('Number')
+    .name('loudness in dBA')
+    .required(false);
+};
