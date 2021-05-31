ffd70941bda92002328184bd757f3b9a071561f2
Brandon Kipp <brandonwkipp@gmail.com>
2021-05-31 10:37:28 -0500

    refactor: Added configuration to prevent giant fontawesome icons on load

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,13 @@
-import propTypes from 'prop-types';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import PropTypes from 'prop-types';
 import React from 'react';
 
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './src/styles/base.scss';
+
+// Disable autoAddCss to help Fontawesome Icons to be reasonable sizes
+config.autoAddCss = false;
 
 // Hack to get Gatsby to stop complaining
 /* eslint import/prefer-default-export: 0 */
@@ -14,5 +19,5 @@ export const wrapRootElement = ({ element }) => (
 );
 
 wrapRootElement.propTypes = {
-  element: propTypes.element.isRequired,
+  element: PropTypes.element.isRequired,
 };
