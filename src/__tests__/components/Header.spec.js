6444a6bce309da9157fdd084008e01b80b397455
Brandon Kipp <brandonwkipp@gmail.com>
2022-01-26 10:10:16 -0600

    testing: Added better link checking tests for Header & Sidebar

--- src/__tests__/components/Header.spec.js
+++ src/__tests__/components/Header.spec.js
@@ -17,16 +17,66 @@ describe('Test <Header>', () => {
     // Ensure top-level nav is present
     expect(screen.getByRole('navigation', { name: 'Primary Navigation' })).toBeInTheDocument();
     expect(screen.getByRole('navigation', { name: 'Primary Navigation' })).toBeVisible();
+  });
+
+  it('<Header> renders links on the home page correctly', async () => {
+    render(<Header path={paths.root} />);
 
     // Check the root link is not displayed
     expect(screen.queryByRole('link', { name: 'Home' })).not.toBeInTheDocument();
 
-    // Check the About link displays correctly
-    expect(screen.queryByRole('link', { name: 'About' })).toBeInTheDocument();
-    expect(screen.queryByRole('link', { name: 'About' })).toBeVisible();
+    // Check the About link is displayed
+    expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'About' })).toBeVisible();
 
-    // Check the Blog link displays correctly
-    expect(screen.queryByRole('link', { name: 'Blog' })).toBeInTheDocument();
-    expect(screen.queryByRole('link', { name: 'Blog' })).toBeVisible();
+    // Check the Blog link is displayed
+    expect(screen.getByRole('link', { name: 'Blog' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Blog' })).toBeVisible();
+  });
+
+  it('<Header> renders links on the about page correctly', async () => {
+    render(<Header path={paths.about} />);
+
+    // Check the About link is not displayed
+    expect(screen.queryByRole('link', { name: 'About' })).not.toBeInTheDocument();
+
+    // Check the root link is displayed
+    expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Home' })).toBeVisible();
+
+    // Check the Blog link is displayed
+    expect(screen.getByRole('link', { name: 'Blog' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Blog' })).toBeVisible();
+  });
+
+  it('<Header> renders links on the blog page correctly', async () => {
+    render(<Header path={paths.blog} />);
+
+    // Check the Blog link is not displayed
+    expect(screen.queryByRole('link', { name: 'Blog' })).not.toBeInTheDocument();
+
+    // Check the root link is displayed
+    expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Home' })).toBeVisible();
+
+    // Check the About link is displayed
+    expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'About' })).toBeVisible();
+  });
+
+  it('<Header> renders links on an unmatched page correctly', async () => {
+    render(<Header path={paths['test-unmatched']} />);
+
+    // Check the root link is displayed
+    expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Home' })).toBeVisible();
+
+    // Check the About link is displayed
+    expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'About' })).toBeVisible();
+
+    // Check the Blog link is displayed
+    expect(screen.getByRole('link', { name: 'Blog' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Blog' })).toBeVisible();
   });
 });
--- src/__tests__/components/Sidebar.spec.js
+++ src/__tests__/components/Sidebar.spec.js
@@ -18,17 +18,76 @@ describe('Test <Sidebar>', () => {
       // Ensure Sidebar nav is present
       expect(screen.getByRole('navigation')).toBeInTheDocument();
       expect(screen.getByRole('navigation')).toBeVisible();
+    });
+  });
 
+  it('<Sidebar> renders links on the home page correctly', async () => {
+    render(<Sidebar path={paths.root} />);
+
+    waitFor(() => {
       // Check the root link is not displayed
       expect(screen.queryByRole('link', { name: 'Home' })).not.toBeInTheDocument();
 
-      // Check the About link displays correctly
-      expect(screen.queryByRole('link', { name: 'About' })).toBeInTheDocument();
-      expect(screen.queryByRole('link', { name: 'About' })).toBeVisible();
+      // Check the About link is displayed
+      expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'About' })).toBeVisible();
 
-      // Check the Blog link displays correctly
-      expect(screen.queryByRole('link', { name: 'Blog' })).toBeInTheDocument();
-      expect(screen.queryByRole('link', { name: 'Blog' })).toBeVisible();
+      // Check the Blog link is displayed
+      expect(screen.getByRole('link', { name: 'Blog' })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'Blog' })).toBeVisible();
+    });
+  });
+
+  it('<Sidebar> renders links on the about page correctly', async () => {
+    render(<Sidebar path={paths.about} />);
+
+    waitFor(() => {
+      // Check the About link is not displayed
+      expect(screen.queryByRole('link', { name: 'About' })).not.toBeInTheDocument();
+
+      // Check the root link is displayed
+      expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'Home' })).toBeVisible();
+
+      // Check the Blog link is displayed
+      expect(screen.getByRole('link', { name: 'Blog' })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'Blog' })).toBeVisible();
+    });
+  });
+
+  it('<Sidebar> renders links on the blog page correctly', async () => {
+    render(<Sidebar path={paths.blog} />);
+
+    waitFor(() => {
+      // Check the Blog link is not displayed
+      expect(screen.queryByRole('link', { name: 'Blog' })).not.toBeInTheDocument();
+
+
+      // Check the root link is displayed
+      expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'Home' })).toBeVisible();
+
+      // Check the About link is displayed
+      expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'About' })).toBeVisible();
+    });
+  });
+
+  it('<Sidebar> renders links on an unmatched page correctly', async () => {
+    render(<Sidebar path={paths['test-unmatched']} />);
+
+    waitFor(() => {
+      // Check the root link is displayed
+      expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'Home' })).toBeVisible();
+
+      // Check the About link is displayed
+      expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'About' })).toBeVisible();
+
+      // Check the Blog link is displayed
+      expect(screen.getByRole('link', { name: 'Blog' })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'Blog' })).toBeVisible();
     });
   });
 });
