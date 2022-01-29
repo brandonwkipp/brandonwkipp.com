2f315434caab1ee9ac97629c77518a4bd2237f65
Brandon Kipp <brandonwkipp@gmail.com>
2022-01-28 23:13:33 -0600

    testing: Completed coverage for DefaultView

--- src/__tests__/views/DefaultView.spec.tsx
+++ src/__tests__/views/DefaultView.spec.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import DefaultView from '@views/DefaultView';
 
 describe('Test <DefaultView>', () => {
+  const ariaLabel = 'Main Content';
   const child = <div>Some Text</div>;
+  const className = 'some-class';
   const path = '/';
 
   it('<DefaultView> renders correctly with minimum props', async () => {
@@ -18,9 +20,30 @@ describe('Test <DefaultView>', () => {
     // TODO: Submit PR to react-burger-menu to add aria-label
     expect(screen.queryAllByRole('navigation')).toHaveLength(2);
 
-    expect(screen.getByRole('main')).toBeInTheDocument();
+    // Ensure <main> is present and displays correctly
+    waitFor(() => {
+      expect(screen.getByRole('main')).toBeInTheDocument();
+      expect(screen.getByRole('main')).toBeVisible();
+      expect(screen.getByRole('main')).toHaveClass('default-view', 'no-height-constraint');
+      expect(screen.getByRole('main')).toHaveTextContent(child.props.children);
+    });
 
     // Ensure <Footer> is present
     expect(screen.getByRole('contentinfo')).toBeInTheDocument();
+  });
+
+  it('<DefaultView> renders correctly with optional props', () => {
+    render(
+      <DefaultView mainAriaLabel={ariaLabel} mainClass={className} noHeightContraint path={path}>
+        {child}
+      </DefaultView>
+    );
+
+    // Check <main> renders classes correctly
+    expect(screen.getByRole('main')).toHaveClass(className);
+    expect(screen.getByRole('main')).not.toHaveClass('no-height-constraint');
+
+    // Check <main> renders aria-label correctly
+    expect(screen.getByRole('main', { name: ariaLabel })).toBeInTheDocument();
   });
 });
