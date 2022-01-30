aea556c0c851b5a6a33c9cae55197f8ddffbd45f
Brandon Kipp <brandonwkipp@gmail.com>
2022-01-29 19:18:32 -0600

    refactor: Removed unused wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,4 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
-import PropTypes from 'prop-types';
-import React from 'react';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,16 +6,3 @@ import './src/styles/base.scss';
 
 // Disable autoAddCss to help Fontawesome Icons to be reasonable sizes
 config.autoAddCss = false;
-
-// Hack to get Gatsby to stop complaining
-/* eslint import/prefer-default-export: 0 */
-
-export const wrapRootElement = ({ element }) => (
-  <div id="wrapper">
-    {element}
-  </div>
-);
-
-wrapRootElement.propTypes = {
-  element: PropTypes.element.isRequired,
-};
