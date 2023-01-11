6e911dcb3204e10e4f001a557196789080bccc48
Brandon W. Kipp <brandonwkipp@gmail.com>
2023-01-11 16:11:27 -0600

    updated tf module location

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_route53_zone" "brandonkipp_com" {
 }
 
 module "brandonwkipp_com" {
-  source = "git@github.com:brandonwkipp/terraform-aws-static-site.git"
+  source = "github.com/brandonwkipp/terraform-aws-static-site"
 
   aws_access_key            = var.aws_access_key
   aws_secret_key            = var.aws_secret_key
